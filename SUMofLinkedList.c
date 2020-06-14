00925f9e96bddbd386e782df3a8a28d281d85ddf
ASHWIN VAZHAPPILLY <ashwinvazhappilly@gmail.com>
2020-06-15 00:41:03 +0530

    Create SUMofLinkedList.c

---
+++ SUMofLinkedList.c
@@ -0,0 +1,67 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+ int data;
+ struct Node *next;
+}*first=NULL;
+void create(int A[],int n)
+{
+ int i;
+ struct Node *t,*last;
+ first=(struct Node *)malloc(sizeof(struct Node));
+ first->data=A[0];
+ first->next=NULL;
+ last=first;
+
+ for(i=1;i<n;i++)
+ {
+ t=(struct Node*)malloc(sizeof(struct Node));
+ t->data=A[i];
+ t->next=NULL;
+ last->next=t;
+ last=t;
+ }
+}
+void Display(struct Node *p)
+{
+ while(p!=NULL)
+ {
+ printf("%d ",p->data);
+ p=p->next;
+ }
+}
+void RDisplay(struct Node *p)
+{
+ if(p!=NULL)
+ {
+ RDisplay(p->next);
+ printf("%d ",p->data);
+
+ }
+}
+
+int add(struct Node *p)
+{ int sum=0;
+  while(p)
+  {
+    sum=sum+p->data;
+    p=p->next;
+  }
+  return sum;
+}
+
+int main()
+{
+ struct Node *temp;
+ int A[]={3,5,7,10,25,8,32,2};
+ create(A,8);
+
+ Display(first);
+ int b;
+ b=add(first);
+ printf("%d",b);
+
+ return 0;
+}
